d93a7758d934682b9eb6f1630d7ba6fb0fb3f3f6
josephkibira <33089347+97joseph@users.noreply.github.com>
2022-03-02 21:39:31 +0300

    Code

---
+++ Code/Code/src/cons.c
@@ -0,0 +1,68 @@
+//
+//  cons.c
+//  2. The Consumer
+//In addition to all tasks defined in the Project 1 document, the consumer will read all encoded
+//frames through the shared pipe, converts all lowercase characters to uppercase and write the
+//modified version of the received data into a file with extension outf.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#define CHUNK 1024
+
+int main(int argc, char *argv[]) {
+    // using shared  pipe comunicates between processes
+    int fd[2];
+    pid_t p;
+    char buf[CHUNK];
+    size_t nread;
+    FILE *fptr;
+    // get filename and its filepath
+    char *fname = argv[1];
+    char *path = realpath(fname,NULL);
+
+    fptr = fopen(fname,"r");
+    if (argc < 3) {
+        fprintf(stderr,"ERROR, a character needed\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (pipe(fd) == -1) {
+        fprintf(stderr, "Pipe Failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("\nfile name is: %s,\nfile path is: %s\n replace character is: %s \n\n",fname,path,argv[2]);
+    
+    p = fork();
+    // use child process to exec producer file
+    
+    if (p == 0){
+        close(fd[0]);
+        // redirect STDOUT_FILENO to fd[1], that contains results from producer
+        dup2(fd[1], STDOUT_FILENO);
+        execl("proc", "proc", path, argv[2], NULL);
+        perror("exec failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    //use parent process to get result form producer file and print
+    wait(NULL);
+    close(fd[1]);
+    dup2(fd[0], STDIN_FILENO);
+    fgets(buf, CHUNK, stdin);
+    if ((fptr = fopen(buf, "r")) == NULL) {
+        perror("fopen failed");
+        exit(EXIT_FAILURE);
+    }
+    // writting to file
+    while ((nread = fread(buf,1,CHUNK,fptr)) > 0)
+        fwrite(buf, 1, nread, stdout);
+    exit(0);
+}
---
+++ Code/Code/src/queue.h
@@ -0,0 +1,27 @@
+//
+//  queue.h
+//  File header files
+
+
+#ifndef __QUEUE_H
+#define __QUEUE_H
+
+typedef struct Queue
+{
+    int capacity;
+    int size;
+    int front;
+    int rear;
+    char **elements;
+} Queue;
+
+
+Queue *createQueue(int capacity);
+int getQueueSize(Queue *queue);
+char *dequeue(Queue *queue);
+void enqueue(Queue *queue, char **element);
+int isQueueFull(Queue *queue);
+int isQueueEmpty(Queue *queue);
+void destroyQueue(Queue *queue);
+
+#endif
